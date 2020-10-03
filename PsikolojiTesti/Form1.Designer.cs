9c00caa5051cb5bcb3d34dfabfde29bfb52b4b17
eren.terlikli <44238447+erenterlikli@users.noreply.github.com>
2020-10-03 19:05:55 +0300

    Add files via upload

---
+++ PsikolojiTesti/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace PsikolojiTesti
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(76, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(654, 189);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(76, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(516, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(214, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(76, 334);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(214, 58);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(516, 334);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(214, 58);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(289, 416);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(214, 58);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(754, 22);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(130, 58);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "BAŞLAT";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(23, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 22);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(750, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Toplam Puan:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(863, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(896, 486);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PsikolojiTesti/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PsikolojiTesti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int soru = 0;
+        int puan = 0;
+
+        private void butonlar()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = true;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button6.Enabled = false;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Text = "Sonraki";
+            soru++;
+            label1.Text = soru.ToString();
+
+            if(soru==1)
+            {
+                textBox1.Text= "Sevgilinizle ya da eşinizle 'mükemmel bir akşam' size aşağıdakilerden hangisini çağırıştırıyor?";
+                button1.Text = "Rock konserine gitmek";
+                button2.Text = "Lunaparka gitmek";
+                button3.Text = "Sinemaya gitmek";
+                button4.Text = "Mum ışığında romantik bir yemek";
+                button5.Text = "Arabayla turlamak";
+
+            }
+
+            if(soru==2)
+            {
+                textBox1.Text = "En sevdiğiniz müzik türü hangisidir?";
+                button1.Text = "Alternatif";
+                button2.Text = "Rock'n'Roll";
+                button3.Text = "Pop";
+                button4.Text = "Soft Rock";
+                button5.Text = "Klasik";
+            }
+
+            if(soru==3)
+            {
+                textBox1.Text = "En çok hangi film türünü seversiniz?";
+                button1.Text = "Korku";
+                button2.Text = "Komedi";
+                button3.Text = "Müzikal";
+                button4.Text = "Romantik";
+                button5.Text = "Belgesel";
+            }
+
+            if(soru==4)
+            {
+                textBox1.Text = "Aşağıdaki mesleklerden birini seçmeniz gerekseydi hangisi olurdunuz?";
+                button1.Text = "Barmen/Barmaid";
+                button2.Text = "Polis";
+                button3.Text = "Öğretmen";
+                button4.Text = "Garson";
+                button5.Text = "Futbolcu";
+            }
+
+            if(soru==5)
+            {
+                textBox1.Text = "Bir saat boş vaktinizi değerlendirmek için hangisi size daha uygun?";
+                button1.Text = "TV izlemek";
+                button2.Text = "İnternete girmek";
+                button3.Text = "Uyumak ";
+                button4.Text = "Okumak";
+                button5.Text = "Dışarıda çalışmak";
+            }
+
+            if(soru==6)
+            {
+                textBox1.Text = "Aşağıdaki renklerden birini seçiniz.";
+                button1.Text = "Sarı";
+                button2.Text = "Kahverengi";
+                button3.Text = "Mavi ";
+                button4.Text = "Kırmızı";
+                button5.Text = "Beyaz";
+            }
+
+            if(soru==7)
+            {
+                textBox1.Text = "Şu an önünüzde aşağıdaki yiyeceklerden hangisi olsa dayanamayıp götürürsünüz?";
+                button1.Text = "Suşi";
+                button2.Text = "Pizza";
+                button3.Text = "Dondurma ";
+                button4.Text = "Makarna";
+                button5.Text = "Salata";
+            }
+
+            if(soru==8)
+            {
+                textBox1.Text = "En sevdiğiniz tatil, bayram ya da özel gün hangisi?";
+                button1.Text = "23 Nisan Çocuk Bayramı";
+                button2.Text = "Yılbaşı Tatili";
+                button3.Text = "Ramazan Bayramı ";
+                button4.Text = "Sevgililer Günü";
+                button5.Text = "Kurban Bayramı";
+            }
+
+            if(soru==9)
+            {
+                textBox1.Text = "Şu an aşağıdakilerden hangisinde olmak isterdiniz?";
+                button1.Text = "Las Vegas'da";
+                button2.Text = "Hawaii'de";
+                button3.Text = "Hollywood'da";
+                button4.Text = "Paris'de";
+                button5.Text = "İspanya'da";
+            }
+
+            if(soru==10)
+            {
+                textBox1.Text = "Nasıl biriyle vakit geçirmekten hoşlanırsınız?";
+                button1.Text = "Bir parti canavarıyla";
+                button2.Text = "Güzel/yakışıklı biriyle";
+                button3.Text = "Komik biriyle";
+                button4.Text = "Duygusal biriyle";
+                button5.Text = "Akıllı biriyle";
+            }
+
+            if(soru==11)
+            {
+                button6.Enabled = false;
+                textBox1.Text = " ";
+                button1.Text = " ";
+                button2.Text = " ";
+                button3.Text = " ";
+                button4.Text = " ";
+                button5.Text = " ";
+                label1.Text = " ";
+
+
+                if(puan>=10 && puan<=17)
+                {
+                    textBox1.Text= "Siz vahşi ve çılgın birisiniz. Eğleneyim derken uçuyorsunuz. Ama her zaman ne yaptığınızın farkındasınız, hayatınızın kontrolünü kaybetmiyorsunuz. İnsanlar olaylara sizin gibi bakmadığında bu sizi durdurmuyor. Tavsiye: Bu vahşi karakterinizle kendinizi ve etrafınızdakileri incitebilirsiniz.Dikkatli olun.";
+                }
+
+                if(puan>= 18 && puan<=26)
+                {
+                    textBox1.Text= "Siz eğlenceli, arkadaş canlısı ve popülersiniz. Cıvıl cıvıl halinizle etrafa neşe saçıyorsunuz. Çogu zaman ikili ilişkilerinizde uçuk kaçık davransanız da inandığınız değerlerden asla taviz vermiyorsunuz. Evlilik ve çocuk sahibi olmak sizin için önemli ama öncelikle yeterince tecrübe edinmek istiyorsunuz. Tavsiye: İnsanların aldığınız kararlarda sizi bu kadar etkilemelerine izin vermeyin.";
+                }
+
+                if(puan>= 27 && puan<=34)
+                {
+                   textBox1.Text= "Öyle tatlısınız ki sizi sevmemek imkansız. Siz insanların asla kaybetmek istemeyeceği türden bir dostsunuz. Kimseyi incitmemek için kendinizi incittiğiniz bile oluyor.Siz genellikle olaylar karşısında olgun ve sakin bir tavır takınırsınız. Tavsiye: Herkes göründüğü gibi değildir, bunu unutmayın.Biraz kurnaz olun.";
+                }
+
+                if(puan>=35 && puan<=42)
+                {
+                    textBox1.Text= "Siz iflah olmaz bir romantiksiniz. şarap, kırmızı güller, bunlar sizin yaşam tarzınız. Söz verirsiniz ve o sözü ne olursa olsun yerine getirirsiniz. Ailenize çok bağlısınız. Annenizi her haftasonu ararsınız, özel günleri asla unutmazsınız. Tavsiye: Fazla romantizmin sizi gerçeklerden uzaklaştırmasına izin vermeyin.";
+                }
+
+                if(puan>=43 && puan<=50)
+                {
+                    textBox1.Text= "Belirgin özelliğiniz hızlı ve doğru karar verebilmeniz. Her olay için kafanızda bir planınız vardır. Fiziksel ve zihinsel gücünüz çok fazla. Mantıklı, ılımlı ve yapıcı tavırlarınızla çoğu kişinin kendine örnek aldığı birisiniz. Tavsiye: Küçük gibi görünen bazı olayları gözardı etmeniz ileride büyük sorunlara yol açabilir.";
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            puan = puan + 1;
+            label3.Text = puan.ToString();
+            butonlar();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            puan = puan + 2;
+            label3.Text = puan.ToString();
+            butonlar();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            puan = puan + 3;
+            label3.Text = puan.ToString();
+            butonlar();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            puan = puan + 4;
+            label3.Text = puan.ToString();
+            butonlar();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            puan = puan + 5;
+            label3.Text = puan.ToString();
+            butonlar();
+        }
+    }
+}
